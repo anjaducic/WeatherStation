b20751042f9894a9f4190cf3693ff10865ace71f
Anja Ducic <ducic.ra5.2020@uns.ac.rs>
2024-10-14 18:14:16 +0200

    feat: Shared library for common classes and interfaces; Weather data model; Weather Station Service Interface

---
+++ WeatherStation/SharedLibrary/CurrentWeatherData.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary
+{
+    [DataContract]
+    public class CurrentWeatherData
+    {
+        [DataMember]
+        public string Location { get => location; set => location = value; }
+        [DataMember]
+        public DateTime Timestamp { get  => timestamp; set => timestamp = value; }
+        [DataMember]
+        public double Temperature { get => temperature; set => temperature = value; }
+        [DataMember]
+        public double Pressure { get => pressure; set => pressure = value; }
+        [DataMember]
+        public double WindSpeed { get => windSpeed; set => windSpeed = value; }
+        [DataMember]
+        public WindDirection WindDirection { get => windDirection; set => windDirection = value; }
+        [DataMember]
+        public double Precipitation { get => precipitation; set => precipitation = value; }
+        [DataMember]
+        public double UVIndex { get => uvIndex; set => uvIndex = value; }
+        [DataMember]
+        public double Humidity { get => humidity; set => humidity = value; }
+
+
+        private string location;
+        private DateTime timestamp;
+        private double temperature;
+        private double pressure;
+        private double windSpeed;
+        private WindDirection windDirection;
+        private double precipitation;
+        private double uvIndex;
+        private double humidity;
+
+        public CurrentWeatherData(string location, DateTime timestamp, double temperature, double pressure, double windSpeed, WindDirection windDirection, double precipitation, double uVIndex, double humidity)
+        {
+            this.location = location;
+            this.timestamp = timestamp;
+            this.temperature = temperature;
+            this.pressure = pressure;
+            this.windSpeed = windSpeed;
+            this.windDirection = windDirection;
+            this.precipitation = precipitation;
+            UVIndex = uVIndex;
+            this.humidity = humidity;
+        }
+
+        public void Update(CurrentWeatherData updated)
+        {
+            this.location = updated.Location;
+            this.timestamp = updated.Timestamp;
+            this.temperature = updated.Temperature;
+            this.pressure = updated.Pressure;
+            this.windSpeed = updated.WindSpeed;
+            this.windDirection = updated.WindDirection;
+            this.precipitation = updated.Precipitation;
+            this.uvIndex = updated.UVIndex;
+            this.humidity = updated.Humidity;
+        }
+    }
+
+    [DataContract]
+    public enum WindDirection
+    {
+        [EnumMemberAttribute]
+        North = 0,
+        [EnumMemberAttribute]
+        NorthEast = 45,
+        [EnumMemberAttribute]
+        East = 90,
+        [EnumMemberAttribute]
+        SouthEast = 135,
+        [EnumMemberAttribute]
+        South = 180,
+        [EnumMemberAttribute]
+        SouthWest = 225,
+        [EnumMemberAttribute]
+        West = 270,
+        [EnumMemberAttribute]
+        NorthWest = 315     
+    }
+
+    
+
+}
---
+++ WeatherStation/SharedLibrary/IWeatherStationService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary
+{
+    [ServiceContract]
+    public interface IWeatherStationService
+    {
+        [OperationContract]
+        void Add(CurrentWeatherData data);
+        [OperationContract]
+        void Edit(CurrentWeatherData data);
+        [OperationContract]
+        CurrentWeatherData Get(DateTime timestamp, string location);
+        [OperationContract]
+        List<CurrentWeatherData> GetInRange(DateTime from, DateTime to, string location);
+        [OperationContract]
+        double GetTemperature(DateTime timestamp, string location);
+        [OperationContract]
+        double GetPressure(DateTime timestamp, string location);
+        [OperationContract]
+        double GetWindSpeed(DateTime timestamp, string location);
+        [OperationContract]
+        WindDirection GetWindDirection(DateTime timestamp, string location);
+        [OperationContract]
+        double GetPrecipitation(DateTime timestamp, string location);
+        [OperationContract]
+        double GetHumidity(DateTime timestamp, string location);
+        [OperationContract]
+        double GetUVIndex(DateTime timestamp, string location);
+        [OperationContract]
+        double GetAverageTemperature(DateTime day, string location);
+        [OperationContract]
+        (double, double) GetMinMaxTemperature(DateTime day, string location);
+        [OperationContract]
+        int GetClearDaysNumber(DateTime month, string location);
+        [OperationContract]
+        int GetRainyDaysNumber(DateTime month, string location);
+        [OperationContract]
+        (double, double) GetMinMaxPrecitipation(DateTime day, string location);
+        [OperationContract]
+        double GetAverageHumidity(DateTime day, string location);
+        [OperationContract]
+        List<int> GetExtremeUVIndexHours(DateTime day, string location);    //u kojim sve satima je bilo extreme
+
+
+
+    }
+}
