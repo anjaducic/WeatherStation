225cbf840ccb286352909f5f283d031dc91dca72
Anja Ducic <ducic.ra5.2020@uns.ac.rs>
2024-10-17 02:46:29 +0200

    feat: Replicator adjusted with the monitor process

--- WeatherStation/Monitor/Program.cs
+++ WeatherStation/Monitor/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.ServiceModel;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using SharedLibrary;
 
 namespace Monitor
@@ -10,141 +14,135 @@ namespace Monitor
         static IServerStateService primary = null;
         static IServerStateService secondary = null;
 
-        static EServerState primaryState = EServerState.Unknown;
-        static EServerState secondaryState = EServerState.Unknown;
-        static bool primaryWorking = false;
-        static bool secondaryWorking = false;
-
         static void Main(string[] args)
         {
+            EServerState primaryState = EServerState.Unknown;
+            EServerState secondaryState = EServerState.Unknown;
+            bool primaryWorking = false;
+            bool secondaryWorking = false;
+
+
             while (true)
             {
-                MonitorPrimaryService();
-                MonitorSecondaryService();
-                UpdateServiceStates();
-                HandleServiceStateSwitch();
-                Thread.Sleep(2000); 
+                try
+                {
+                    ChannelFactory<IServerStateService> cfPrimary = new ChannelFactory<IServerStateService>("Primary");
+                    primary = cfPrimary.CreateChannel();
+
+                    if ((secondaryState != EServerState.Primary && primaryWorking) || !secondaryWorking)
+                    {
+                        primary.UpdateState(EServerState.Primary);
+                    }
+                    else if (primaryWorking && secondaryWorking) 
+                    {
+                        primary.UpdateState(EServerState.Secondary);    //ako se vrati
+                        primaryState = EServerState.Secondary;
+                    }
+                    primaryWorking = true;
+                }
+                catch (CommunicationException cex)
+                {
+                    Console.WriteLine("Primary service unavailable. Reason: " + cex.Message);
+                    primaryState = EServerState.Unknown;
+                    primaryWorking = false;
+                }
+
+
+                try
+                {
+                    ChannelFactory<IServerStateService> cfSecondary = new ChannelFactory<IServerStateService>("Secondary");
+                    secondary = cfSecondary.CreateChannel();
+
+                    if (secondaryState == EServerState.Primary && secondaryWorking && primaryState != EServerState.Primary)
+                    {
+                        secondary.UpdateState(EServerState.Primary);
+                    }
+                        
+                    else if (primaryWorking && secondaryWorking)
+                    {
+                        secondary.UpdateState(EServerState.Secondary);
+                        secondaryState = EServerState.Secondary;
+                    }
+                    secondaryWorking = true;
+                        
+                }
+                catch (CommunicationException cex)
+                {
+                    Console.WriteLine("Secondary service unavailable. Reason: " + cex.Message);
+                    secondaryState = EServerState.Unknown;
+                    secondaryWorking = false;
+                }
+
+                
+
+                try
+                {
+                    primaryState = primary.GetState();
+                    primaryWorking = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("SERVICE-A ERROR: " + ex.Message);
+                    primaryState = EServerState.Unknown;
+                    primaryWorking = false;
+                }
+                try
+                {
+                    secondaryState = secondary.GetState();
+                    secondaryWorking = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("SERVICE-B ERROR: " + ex.Message);
+                    secondaryState = EServerState.Unknown;
+                    secondaryWorking = false;
+                }
+
+
+//****************************************************
+
+                if (primaryState == EServerState.Unknown)
+                {
+                    Thread.Sleep(500);
+
+                    if (secondaryState != EServerState.Primary)
+                    {
+                        try
+                        {
+                            secondary.UpdateState(EServerState.Primary);
+                            secondaryState = EServerState.Primary;
+                            Console.WriteLine("SERVICE-B STATUS BECAME: PRIMARY");
+                            secondaryWorking = true;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("SERVICE-B ERROR SWITCHING STATE: " + ex.Message);
+                            secondaryState = EServerState.Unknown;
+                            secondaryWorking = false;
+                        }
+                        
+                    }
+                    else if (secondaryState == EServerState.Unknown)
+                    {
+                        Console.WriteLine("BOTH SERVICES UNAVAILABLE. WAITING...");
+                        
+                    }
+                }
+
+                if (secondaryState == EServerState.Unknown)
+                {
+                    Console.WriteLine("SERVICE-B ERROR.");
+                }
+
+                Console.WriteLine("SERVICES STATES.\n");
+                Console.WriteLine("-primary: " + primaryState.ToString());
+                Console.WriteLine("-secondary: " + secondaryState.ToString());
+
+
+                Thread.Sleep(2000);
             }
+
         }
 
-        static void MonitorPrimaryService()
-        {
-            try
-            {
-                ChannelFactory<IServerStateService> cfPrimary = new ChannelFactory<IServerStateService>("Primary");
-                primary = cfPrimary.CreateChannel();
-
-                if ((secondaryState != EServerState.Primary && primaryWorking) || !secondaryWorking)
-                {
-                    primary.UpdateState(EServerState.Primary);
-                }
-                else if (primaryWorking && secondaryWorking)
-                {
-                    primary.UpdateState(EServerState.Secondary); // Ako se vrati
-                    primaryState = EServerState.Secondary;
-                }
-                primaryWorking = true;
-            }
-            catch (CommunicationException cex)
-            {
-                Console.WriteLine("Primary service unavailable. Reason: " + cex.Message);
-                primaryState = EServerState.Unknown;
-                primaryWorking = false;
-            }
-        }
-
-        static void MonitorSecondaryService()
-        {
-            try
-            {
-                ChannelFactory<IServerStateService> cfSecondary = new ChannelFactory<IServerStateService>("Secondary");
-                secondary = cfSecondary.CreateChannel();
-
-                if (secondaryState == EServerState.Primary && secondaryWorking && primaryState != EServerState.Primary)
-                {
-                    secondary.UpdateState(EServerState.Primary);
-                }
-                else if (primaryWorking && secondaryWorking)
-                {
-                    secondary.UpdateState(EServerState.Secondary);
-                    secondaryState = EServerState.Secondary;
-                }
-                secondaryWorking = true;
-            }
-            catch (CommunicationException cex)
-            {
-                Console.WriteLine("Secondary service unavailable. Reason: " + cex.Message);
-                secondaryState = EServerState.Unknown;
-                secondaryWorking = false;
-            }
-        }
-
-        static void UpdateServiceStates()
-        {
-            try
-            {
-                primaryState = primary.GetState();
-                primaryWorking = true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("SERVICE-A ERROR: " + ex.Message);
-                primaryState = EServerState.Unknown;
-                primaryWorking = false;
-            }
-
-            try
-            {
-                secondaryState = secondary.GetState();
-                secondaryWorking = true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("SERVICE-B ERROR: " + ex.Message);
-                secondaryState = EServerState.Unknown;
-                secondaryWorking = false;
-            }
-        }
-
-        static void HandleServiceStateSwitch()
-        {
-            if (primaryState == EServerState.Unknown)
-            {
-                if (secondaryState == EServerState.Secondary)
-                {
-                    TrySwitchToPrimary();
-                }
-                else if (secondaryState == EServerState.Unknown)
-                {
-                    Console.WriteLine("BOTH SERVICES UNAVAILABLE. WAITING...");
-                }
-            }
-
-            if (secondaryState == EServerState.Unknown)
-            {
-                Console.WriteLine("SERVICE-B ERROR.");
-            }
-
-            Console.WriteLine("SERVICES STATES.\n");
-            Console.WriteLine("-primary: " + primaryState.ToString());
-            Console.WriteLine("-secondary: " + secondaryState.ToString());
-        }
-
-        static void TrySwitchToPrimary()
-        {
-            try
-            {
-                secondary.UpdateState(EServerState.Primary);
-                secondaryState = EServerState.Primary;
-                Console.WriteLine("SERVICE-B STATUS BECAME: PRIMARY");
-                secondaryWorking = true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("SERVICE-B ERROR SWITCHING STATE: " + ex.Message);
-                secondaryState = EServerState.Unknown;
-                secondaryWorking = false;
-            }
-        }
     }
 }
--- WeatherStation/Replicator/Program.cs
+++ WeatherStation/Replicator/Program.cs
@@ -11,6 +11,12 @@ namespace Replicator
 {
     public class Program
     {
+        static IServerStateService channelPrimary = null;
+        static IServerStateService channelSecondary = null;
+        static IWeatherStationService channelSource = null;
+        static IWeatherStationService channelDestination = null;
+        static EServerState primaryState = EServerState.Unknown;
+        static EServerState secondaryState = EServerState.Unknown;
         static void Main(string[] args)
         {
             DateTime lastReplicationTime = DateTime.MinValue;
@@ -22,15 +28,65 @@ namespace Replicator
                 {
                     ChannelFactory<IWeatherStationService> cfSource = new ChannelFactory<IWeatherStationService>("Source");
                     ChannelFactory<IWeatherStationService> cfDestination = new ChannelFactory<IWeatherStationService>("Destination");
-                    IWeatherStationService channelSource = cfSource.CreateChannel();
-                    IWeatherStationService channelDestination = cfDestination.CreateChannel();
-                    channelDestination.EnterData(channelSource.GetSince(lastReplicationTime));
-                    channelDestination.GetSince(lastReplicationTime);    //samo da testiram
-                    lastReplicationTime = DateTime.Now;
+                    channelSource = cfSource.CreateChannel();
+                    channelDestination = cfDestination.CreateChannel();
 
-                    Thread.Sleep(3000);
+                    ChannelFactory<IServerStateService> cfPrimary = new ChannelFactory<IServerStateService>("Primary");
+                    ChannelFactory<IServerStateService> cfSecondary = new ChannelFactory<IServerStateService>("Secondary");
+                    channelPrimary = cfPrimary.CreateChannel();
+                    channelSecondary = cfSecondary.CreateChannel();
+
                 }
                 catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+
+                try
+                {
+                    primaryState = channelPrimary.GetState();
+                }
+                catch
+                {
+                    Console.WriteLine("SERVICE-A NOT AVAILABLE.");
+                    primaryState = EServerState.Unknown;
+                }
+
+
+                try
+                {
+                    secondaryState = channelSecondary.GetState();
+                }
+                catch
+                {
+                    Console.WriteLine("SERVICE-B NOT AVAILABLE");
+                    secondaryState = EServerState.Unknown;
+                }
+
+                try
+                {
+                    if(primaryState == EServerState.Primary && secondaryState != EServerState.Primary)
+                    {
+                        channelDestination.EnterData(channelSource.GetSince(lastReplicationTime));
+                        channelDestination.GetSince(lastReplicationTime);    //for testing
+                        lastReplicationTime = DateTime.Now;                 //refresh if replication is successful
+
+                        Console.WriteLine("REPLICATION A -> B\n");
+                        Thread.Sleep(3000);
+                    }
+                    else if (secondaryState == EServerState.Primary && primaryState != EServerState.Primary) 
+                    {
+                        channelSource.EnterData(channelDestination.GetSince(lastReplicationTime));
+                        channelSource.GetSince(lastReplicationTime);   
+                        lastReplicationTime = DateTime.Now;
+
+                        Console.WriteLine("REPLICATION B -> A\n");
+                        Thread.Sleep(3000);
+                    }
+                    
+                }
+                catch (Exception ex) 
                 {
                     Console.WriteLine(ex.Message);
                 }
